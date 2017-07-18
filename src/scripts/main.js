6902cdf1385206c23eb7274a24fcc2445be73a98
Craig J Duffy <cd21@hw.ac.uk>
2017-07-18 11:04:13 +0100

    New refill style (more labels!) and less garish line options

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
-		
-
 /*	var geocoderOptions = {
 	expanded: true,
 	position: 'topleft',
@@ -10,45 +8,58 @@
 //var geocoder = L.Mapzen.geocoder(geocoderOptions);
 //geocoder.addTo(map);
 var routingControl = L.Mapzen.routing.control({
-	/*waypoints: [
-		L.latLng(55.909795, -3.318300),
-		L.latLng(55.942746, -3.215559)
-	],*/
-	lineOptions: {
-	styles: [ {color:"#FE0000",opacity:0.50, weight: 16},
-		{color:"#FF6300", opacity:1, weight:12},
-		{color:"#FFFF01",opacity:1, weight:8},
-		{color:"#008001", opacity:1, weight:6},
-		{color:"#0000FE", opacity:1, weight:4},
-		{color:"#4B0081", opacity:1, weight:2},
-		{color:"#BC31FE", opacity:1, weight:1}]
-	},
-	position: 'topleft',
-	geocoder: L.Mapzen.routing.geocoder(),
-	router: L.Mapzen.routing.router({costing:"bicycle",costing_options:{bicycle:{use_roads:"1",use_hills:"0"}}}),
-	summaryTemplate:'<div class="start">{name}</div><div class="info {costing}">{distance}, {time}</div>',
-	routeWhileDragging: false,
-	fitSelectedRoutes:true}).addTo(map);
-	L.Mapzen.routing.errorControl(routingControl).addTo(map);
+  /*waypoints: [
+  	L.latLng(55.909795, -3.318300),
+  	L.latLng(55.942746, -3.215559)
+  ],*/
+  lineOptions: {
+    styles: [{
+        color: "white",
+        opacity: 0.8,
+        weight: 12
+      }, //keep it simple for now
+      {
+        color: "#2676C6",
+        opacity: 1,
+        weight: 6
+      }
+    ]
+  },
+  position: 'topleft',
+  geocoder: L.Mapzen.routing.geocoder(),
+  router: L.Mapzen.routing.router({
+    costing: "bicycle",
+    costing_options: {
+      bicycle: {
+        use_roads: "1",
+        use_hills: "0"
+      }
+    }
+  }),
+  summaryTemplate: '<div class="start">{name}</div><div class="info {costing}">{distance}, {time}</div>',
+  routeWhileDragging: false,
+  fitSelectedRoutes: true
+}).addTo(map);
+L.Mapzen.routing.errorControl(routingControl).addTo(map);
 
 routingControl.addEventListener("routesfound", requestElevationData);
 
 function requestElevationData(e) {
 
-	var data = polyline.encode(e.routes[0].coordinates, 6);
+  var data = polyline.encode(e.routes[0].coordinates, 6);
 
-	//var data = stringify(e.routes[0]);
-	var xhttp = new XMLHttpRequest();
-	data = "?json={\"range\":true, \"encoded_polyline\":\"".concat(data, "\"}&api_key=mapzen-bTyRhDo");
-	xhttp.onreadystatechange = receiveElevationData(this);
-	xhttp.open("GET", "http://elevation.mapzen.com/height".concat(data), true);
-	xhttp.send();			
+  //var data = stringify(e.routes[0]);
+  var xhttp = new XMLHttpRequest();
+  data = "?json={\"range\":true, \"encoded_polyline\":\"".concat(data, "\"}&api_key=mapzen-bTyRhDo");
+  xhttp.onreadystatechange = receiveElevationData(this);
+  xhttp.open("GET", "http://elevation.mapzen.com/height".concat(data), true);
+  xhttp.send();
 }
 
-function receiveElevationData(xhttp){
-	
-		//alert("!");
-	
+function receiveElevationData(xhttp) {
+
+  //alert("!");
+
 }
 
 //function stringify(IRoute) {
@@ -65,4 +76,3 @@ function receiveElevationData(xhttp){
 
 //	return string;
 //}
-
