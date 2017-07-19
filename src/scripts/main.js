04600483aa78eea6d4838a66e6596a2a67b682f0
Tommy Lamb <james003pond@gmail.com>
2017-07-19 15:50:23 +0100

    Untested groundwork. Untested groundwork towards the grouping of gradient segments to form individual hills.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+		
+
 /*	var geocoderOptions = {
 	expanded: true,
 	position: 'topleft',
@@ -8,71 +10,92 @@
 //var geocoder = L.Mapzen.geocoder(geocoderOptions);
 //geocoder.addTo(map);
 var routingControl = L.Mapzen.routing.control({
-  /*waypoints: [
-  	L.latLng(55.909795, -3.318300),
-  	L.latLng(55.942746, -3.215559)
-  ],*/
-  lineOptions: {
-    styles: [{
-        color: "white",
-        opacity: 0.8,
-        weight: 12
-      }, //keep it simple for now
-      {
-        color: "#2676C6",
-        opacity: 1,
-        weight: 6
-      }
-    ]
-  },
-  position: 'topleft',
-  geocoder: L.Mapzen.routing.geocoder(),
-  router: L.Mapzen.routing.router({
-    costing: "bicycle",
-    costing_options: {
-      bicycle: {
-        use_roads: "1",
-        use_hills: "0"
-      }
-    }
-  }),
-  summaryTemplate: '<div class="start">{name}</div><div class="info {costing}">{distance}, {time}</div>',
-  routeWhileDragging: false,
-  fitSelectedRoutes: true
-}).addTo(map);
-L.Mapzen.routing.errorControl(routingControl).addTo(map);
+	/*waypoints: [
+		L.latLng(55.909795, -3.318300),
+		L.latLng(55.942746, -3.215559)
+	],*/
+	lineOptions: {
+		styles: [{
+			color: "white",
+			opacity: 0.8,
+			weight: 12
+		}, //keep it simple for now
+		{
+			color: "#2676C6",
+			opacity: 1,
+			weight: 6
+		}
+		]
+	},
+	position: 'topleft',
+	geocoder: L.Mapzen.routing.geocoder(),
+	router: L.Mapzen.routing.router({costing:"bicycle",costing_options:{bicycle:{use_roads:"1",use_hills:"0"}}}),
+	summaryTemplate:'<div class="start">{name}</div><div class="info {costing}">{distance}, {time}</div>',
+	routeWhileDragging: false,
+	fitSelectedRoutes:true}).addTo(map);
+	L.Mapzen.routing.errorControl(routingControl).addTo(map);
 
 routingControl.addEventListener("routesfound", requestElevationData);
 
+
+var lowerHillBound = 5;
+var positiveGradientUncertainty = 3;
+var negativeGradientUncertainty = 3;
+
 function requestElevationData(e) {
 
-  var data = polyline.encode(e.routes[0].coordinates, 6);
+	var data = polyline.encode(e.routes[0].coordinates, 6);
 
-  //var data = stringify(e.routes[0]);
-  var xhttp = new XMLHttpRequest();
-  data = "?json={\"range\":true, \"encoded_polyline\":\"".concat(data, "\"}&api_key=mapzen-bTyRhDo");
-  xhttp.onreadystatechange = receiveElevationData(this);
-  xhttp.open("GET", "http://elevation.mapzen.com/height".concat(data), true);
-  xhttp.send();
+	var xhttp = new XMLHttpRequest();
+	data = "?json={\"range\":true, \"encoded_polyline\":\"".concat(data, "\"}&api_key=mapzen-bTyRhDo");
+	xhttp.onreadystatechange = function () {
+		receiveElevationData(xhttp);
+	};
+	xhttp.open("GET", "http://elevation.mapzen.com/height".concat(data), true);
+	xhttp.send();			
 }
 
 function receiveElevationData(xhttp) {
 
-  //alert("!");
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		processElevationData(JSON.parse(xhttp.responseText));
+	}
 
 }
 
-//function stringify(IRoute) {
+//The data returned appears to be in meters.
+function processElevationData(data) {
+	var previousGrad;
+	var hillStartDistance;
+	var totalXDistance = 0;
+	var totalYHeight = 0;
 
-//	var string = "[";
+	for (index = 1; index < data.range_height.length; index++) {
 
-//	IRoute.coordinates.forEach(function (entry, index) {
-//		string = string.concat("{\"lat\":".concat(entry.lat, ", \"lon\":", entry.lng, "}"), ",");
-//	})
+		var deltaY = (data.range_height[index][1] - data.range_height[index - 1][1]);
+		var deltaX = (data.range_height[index][0] - data.range_height[index - 1][0]);
+		var gradient = (deltaY / deltaX) * 100; //Gradient as a percentage
 
-//	string = string.slice(0, -1).concat("]");
+		console.log(index + " | "+ gradient);
 
-//	alert(string);
+		if (gradient > lowerHillBound) {//If greater than lower bound, start/continue a hill segment
 
-//	return string;
-//}
+			if (typeof (previousGrad) == 'undefined' || previousGrad < lowerHillBound) { //Start a new hill segment
+				previousGrad = gradient;
+				hillStartDistance = data.range_height[index - 1][0];
+				totalXDistance += deltaX;
+				totalYHeight += deltaY
+			} else if (gradient < previousGrad + positiveGradientUncertainty && gradient > previousGrad - negativeGradientUncertainty) {// Continue hill segment
+
+			} else { 
+				//end current segment and start a new one.
+				//End segment due to sharp change in gradient
+				//Start new segment as gradient is still considered a hill (above lower bound).
+			}
+		} else {
+			//End current segment if applicable, as we are now nominally flat.
+		}
+	}
+}
+
+
