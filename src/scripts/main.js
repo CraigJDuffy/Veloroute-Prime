e4aefc245b120f692c3a6629ba4e5734ca703695
Tommy Lamb <james003pond@gmail.com>
2017-08-14 16:38:00 +0100

    Marker UI for gradient warnings

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,12 +21,60 @@ var routingControl = L.Mapzen.routing.control({
 }).addTo(map);
 L.Mapzen.routing.errorControl(routingControl).addTo(map);
 
-routingControl.addEventListener("routesfound", requestElevationData);
+var redHillIcon = L.icon({
+    iconUrl: 'media/redHill.svg',
+    iconSize: [32, 48],
+    iconAnchor: [16, 48],
+    popupAnchor: [16, 0]
+});
+
+var orangeHillIcon = L.icon({
+    iconUrl: 'media/orangeHill.svg',
+    iconSize: [32, 48],
+    iconAnchor: [16, 48],
+    popupAnchor: [16, 0]
+});
+
+var yellowHillIcon = L.icon({
+    iconUrl: 'media/yellowHill.svg',
+    iconSize: [32, 48],
+    iconAnchor: [16, 48],
+    popupAnchor: [16, 0]
+});
+
+var greenHillIcon = L.icon({
+    iconUrl: 'media/greenHill.svg',
+    iconSize: [32, 48],
+    iconAnchor: [16, 48],
+    popupAnchor: [16, 0]
+});
+
+routingControl.addEventListener("routesfound", routesFound);
+routingControl.getPlan().addEventListener("waypointschanged", waypointsChanged);
 
 
 var lowerHillBound = 6;
 var positiveGradientUncertainty = 14;
 var negativeGradientUncertainty = 14;
+var latestRoute;
+var hillMarkers = [];
+
+function routesFound(e) {
+    requestElevationData(e);
+    latestRoute = e.routes[0];
+}
+
+function waypointsChanged(e) {
+
+    if (typeof(e.waypoints[0].latlng) == 'undefined' || typeof(e.waypoints[1].latlng) == 'undefined') { //For some reason the array always has at least two elements, either or both of which may have null attributes.
+
+        for (i = 0; i < hillMarkers.length; i++) { //remove each marker
+            hillMarkers[i].remove();
+        }
+
+        hillMarkers = [];
+    }
+}
 
 function requestElevationData(e) {
 
@@ -49,22 +97,26 @@ function receiveElevationData(xhttp) {
 
 }
 
-function rankHill(dY, dX, hillStartDistance) {
+function rankHill(dY, dX, hillStartIndex) {
     var area = (0.5) * dY * dX; // area of triangle
     //console.log("Area:" + area);
 
     switch (gradeHill(area)) {
         case 1:
-            console.log("Insignificant hill at: " + hillStartDistance);
+            //console.log("Insignificant hill at: " + hillStartIndex);
+            hillMarkers.push(L.marker(latestRoute.coordinates[hillStartIndex], { icon: greenHillIcon, riseOnHover: true, bubblingMouseEvents: true }).addTo(map));
             break;
         case 2:
-            console.log("Average hill at: " + hillStartDistance);
+            //console.log("Average hill at: " + hillStartIndex);
+            hillMarkers.push(L.marker(latestRoute.coordinates[hillStartIndex], { icon: yellowHillIcon, riseOnHover: true, bubblingMouseEvents: true }).addTo(map));
             break;
         case 3:
-            console.log("Potentially Challenging hill at: " + hillStartDistance);
+            //console.log("Potentially Challenging hill at: " + hillStartIndex);
+            hillMarkers.push(L.marker(latestRoute.coordinates[hillStartIndex], { icon: orangeHillIcon, riseOnHover: true, bubblingMouseEvents: true }).addTo(map));
             break;
         case 4:
-            console.log("Challenging hill at: " + hillStartDistance);
+            //console.log("Challenging hill at: " + hillStartIndex);
+            hillMarkers.push(L.marker(latestRoute.coordinates[hillStartIndex], { icon: redHillIcon, riseOnHover: true, bubblingMouseEvents: true }).addTo(map));
             break;
         default:
         //console.log("Error reciving hill");
@@ -72,22 +124,24 @@ function rankHill(dY, dX, hillStartDistance) {
 }
 
 function gradeHill(area) {
-    var lvl1 = 20;
-    var lvl2 = 30;
-    var lvl3 = 50;
-    var lvl4 = 70;
+    var lvl1 = 5;
+    var lvl2 = 20;
+    var lvl3 = 40;
+    var lvl4 = 60;
 
-    if (area < lvl2) {
-        return 1;
-    }
-    else if (area < lvl3) {
-        return 2;
-    }
-    else if (area < lvl4) {
-        return 3;
-    }
-    else {
-        return 0;
+    if (area > lvl1) {
+        if (area < lvl2) {
+            return 1;
+        }
+        else if (area < lvl3) {
+            return 2;
+        }
+        else if (area < lvl4) {
+            return 3;
+        }
+        else {
+            return 4;
+        }
     }
 }
 
@@ -95,13 +149,9 @@ function gradeHill(area) {
 //The data returned appears to be in meters.
 function processElevationData(data) {
     var previousGrad;
-    var hillStartDistance;
-    var hillBaseHeight;
-
-    var range_coords = polyline.getRange_Coords();
-
-    //console.log(data.range_height.length + " | " + range_coords.length);
-    //console.log("0 | " + data.range_height[0][0] + " | " + range_coords[0][0]);
+    //var hillStartDistance;
+    //var hillBaseHeight;
+    var hillStartIndex;
 
     for (index = 1; index < data.range_height.length; index++ , previousGrad = gradient) {
 
@@ -109,29 +159,28 @@ function processElevationData(data) {
         var deltaX = (data.range_height[index][0] - data.range_height[index - 1][0]);
         var gradient = (deltaY / deltaX) * 100; //Gradient as a percentage
 
-        //console.log(index + " | " + data.range_height[index][0] + " | " + range_coords[index][0]);
+        //console.log(index + " | " + gradient);
 
         if (gradient > lowerHillBound) {//If greater than lower bound, start/continue a hill segment
 
             if (typeof (previousGrad) == 'undefined' || previousGrad < lowerHillBound) { //Start a new hill segment
 
-                hillStartDistance = data.range_height[index - 1][0];
-                hillBaseHeight = data.range_height[index - 1][1];
-
-                //console.log("Start Hill");
-                //console.log(index + " | "+ gradient);
+                //hillStartDistance = data.range_height[index - 1][0];
+                //hillBaseHeight = data.range_height[index - 1][1];
+                hillStartIndex = index - 1;
 
             } else if (gradient < previousGrad + positiveGradientUncertainty && gradient > previousGrad - negativeGradientUncertainty) {// Continue hill segment
 
                 //console.log("Continue Hill");
 
             } else {
-                rankHill(deltaY, deltaX, hillStartDistance);
+                rankHill(deltaY, deltaX, hillStartIndex);
 
                 //console.log("Restart Hill");
 
-                hillStartDistance = data.range_height[index - 1][0];
-                hillBaseHeight = data.range_height[index - 1][1];
+                //hillStartDistance = data.range_height[index - 1][0];
+                //hillBaseHeight = data.range_height[index - 1][1];
+                hillStartIndex = index - 1;
 
                 //end current segment and start a new one.
                 //End segment due to sharp change in gradient
@@ -143,7 +192,7 @@ function processElevationData(data) {
             //End current segment if applicable, as we are now nominally flat (or downhill).
 
             if (previousGrad > lowerHillBound) {
-                rankHill(deltaY, deltaX, hillStartDistance);
+                rankHill(deltaY, deltaX, hillStartIndex);
                 //console.log("End Hill");
             }
 
